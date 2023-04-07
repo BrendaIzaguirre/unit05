88c1599dda4305faf99a77332ca1a2f0674f965b
BrendaIzaguirre <112352653+BrendaIzaguirre@users.noreply.github.com>
2023-04-07 12:10:47 -0500

    Update fallingDistance.kt

--- fallingDistance.kt
+++ fallingDistance.kt
@@ -1,9 +1,8 @@
 fun main() {
     fun fallingDistance(t: Double): Double {
-        val g = 9.8
-        return 0.5 * g * t * t
+        val distance g = 9.8
+        return distance 0.5 * g * t * t
     }
-
             for (i in 1..10) {
             println("The object has fallen ${fallingDistance(i.toDouble())} meters in $i second(s).")
         }
